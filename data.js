3a73557ebbf344db06920070373770c61ee82f0b
ECHOX-EXTRA <zachvanderbecq@gmail.com>
2025-06-03 16:27:37 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const vehicles = [
+  {
+    name: "Lamborghini Aventador",
+    spawn: "aventador",
+    speed: 350,
+    category: "Super",
+    image: "https://cdn.discordapp.com/attachments/1228018480359487528/1233788104381376552/lamborghini_aventador.png"
+  },
+  {
+    name: "Bugatti Chiron",
+    spawn: "chiron",
+    speed: 400,
+    category: "Hypercar",
+    image: "https://cdn.discordapp.com/attachments/1228018480359487528/1233791312725202994/bugatti_chiron.png"
+  },
+  {
+    name: "Nissan GTR R35",
+    spawn: "gtr",
+    speed: 320,
+    category: "Sport",
+    image: "https://cdn.discordapp.com/attachments/1228018480359487528/1233795135513614428/nissan_gtr.png"
+  }
+];
